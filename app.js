308b67dcbe034a3238ba09c5dd75b88fdaf68525
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-08 14:12:31 +0545

    Connect mongo db connection

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+const connectDB = require('./db/connect');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 
+// Port Number
 const port = 8000;
-
 
 // middle ware 
 app.use('/api/v1/tasks',tasks);
 
+// Connect to our database
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}....`);
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}....`);
-});
+start();
+
+
+
